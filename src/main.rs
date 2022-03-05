cb88b78f85d4a3191120d53fca2580078c0123eb
four18teapot <JoshuaHoffmann@protonmail.com>
2022-03-05 19:07:28 +0100

    Tracking

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::i64::MAX;
 
-type Vec3D = (i16, i16, i16);
+type Vec3D = (i32, i32, i32);
 
 const MOVES: [Vec3D; 24] = [(2,1,0), (2,-1,0), (-2,1,0), (-2,-1,0), (2,0,1), (2,0,-1), (-2,0,1), (-2,0,-1), (0,2,1), (0,2,-1), (0,-2,1), (0,-2,-1),(0,1,2), (0,1,-2), (0,-1,2), (0,-1,-2),(1,0,2), (1,0,-2), (-1,0,2), (-1,0,-2),(1,2,0), (1,-2,0), (-1,2,0), (-1,-2,0)];
 
@@ -9,7 +9,7 @@ fn add(a: &Vec3D, b: &Vec3D) -> Vec3D {
     return (a.0 + b.0, a.1 + b.1, a.2 + b.2);
 }
 
-fn abs(a: &Vec3D) -> i16 {
+fn abs(a: &Vec3D) -> i32 {
     return (a.0 * a.0 + a.1 * a.1 + a.2 * a.2)
 }
 
@@ -44,7 +44,7 @@ fn tour(max_iter: i64) -> Option<i64> {
     let mut m = 0;
     
     loop {
-	
+
         tour.insert(current);
 	
         current = match jump(&current, &tour) {
@@ -61,22 +61,20 @@ fn tour(max_iter: i64) -> Option<i64> {
 
         m += 1;
 
-        if m % 10000000 == 0 {
-	   println!("");
-           println!("Iteration: {}", m);
-	   println!("Currently at: {}, {}, {}", current.0, current.1, current.2);
-           print!("Garbage collection: {}", tour.len());
+	
+        if m % 10000 == 0 {
+	   print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+	   println!("Iteration: {} \t\t At: {} {} {} \t\t ABS: {}", m, current.0, current.1, current.2, (abs(&current) as f32).sqrt());
            // Garbage collection
            let mut exclude:Vec<Vec3D> = Vec::new();
            for sq in tour.iter() {
                if is_surrounded(sq, &tour) {
                     exclude.push(*sq);
-                }
+               }
            }
            for r in exclude.iter() {	        
                tour.remove(r);
 	   }
-	   println!(" -> {}", tour.len());
 	}
     }
 
